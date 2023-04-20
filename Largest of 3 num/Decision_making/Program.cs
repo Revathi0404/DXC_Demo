acd34e823e415697cf9283fbed6962615f734cf3
T <revathi.t@dxc.com>
2023-04-20 12:41:09 +0530

    updated

---
+++ Largest of 3 num/Decision_making/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Decision_making
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter num1");
+            int num1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter num2");
+            int num2 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter num3");
+            int num3 = Convert.ToInt16(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.WriteLine("Number one is the largest!");
+                }
+                else
+                {
+                    Console.WriteLine("Number three is the largest!");
+         }
+            }
+            else if (num2 > num3)
+                Console.WriteLine("Number two is the largest!");
+            else
+                Console.WriteLine("Number three is the largest!");
+
+        }
+    }
+}
---
+++ Vowel_consonant/Vowel_consonant/Program.cs
@@ -0,0 +1,14 @@
+﻿
+        Console.WriteLine("Enter the Alphabet");
+        Char ch=Convert.ToChar(Console.ReadLine());
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            {
+            Console.WriteLine("Vowel");
+            }     
+        else
+            {
+            Console.WriteLine("Consonant");
+            }
+
+            
+    
